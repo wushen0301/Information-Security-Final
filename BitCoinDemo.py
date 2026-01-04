61666a5ee9081c931474e102ccf5b677427e0d57
wushen0301 <wushen0301@gmail.com>
2026-01-04 14:23:11 +0800

    Update code

--- BitCoinDemo.py
+++ BitCoinDemo.py
@@ -1,6 +1,7 @@
 import hashlib
 import time
 import tkinter as tk
+import random
 from tkinter import scrolledtext
 
 class BlockchainSimulator:
@@ -9,7 +10,7 @@ class BlockchainSimulator:
         self.root.title("Bitcoin PoW 挖礦模擬器")
         self.root.geometry("800x650")
         
-        #預設一個 Previous Hash
+        #預設一個Previous Hash
         self.prev_hash_val = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
         
         self.create_widgets()
@@ -68,37 +69,38 @@ class BlockchainSimulator:
         self.log(f"目標難度: 左邊有 {difficulty} 個零")
         self.log(f"區塊內容: {transactions}")
         
-        nonce = 0
+        nonce = random.randint(0, 1000000000)   #起始nonce
+        attempts = 1    #計算次數
         start_time = time.time()
         
         while True:
-            #串接字串 (PrevHash + Transactions + Nonce)
+            #串連(PrevHash + Transactions + Nonce)
             content = f"{prev_hash}{transactions}{nonce}"
 
             #做SHA-256
             new_hash = hashlib.sha256(content.encode()).hexdigest()
             
-            #檢查是否符合難度要求
+            #成功找到符合的
             if new_hash.startswith(prefix):
 
-                #成功找到符合的
                 end_time = time.time()
                 elapsed_time = end_time - start_time
                 
                 self.log("-" * 50)
-                self.log(f"成功找到符合條件的 Nonce!")
-                self.log(f"最終 Nonce: {nonce}")
-                self.log(f"最終 Hash: {new_hash}")
-                self.log(f"運算次數: {nonce + 1} 次")
-                self.log(f"耗費時間: {elapsed_time:.4f} 秒")
+                self.log(f"成功找到符合條件的Nonce!")
+                self.log(f"- Nonce: {nonce}")
+                self.log(f"- Hash: {new_hash}")
+                self.log(f"- 總次數: {attempts} 次")
+                self.log(f"- 花費時間: {elapsed_time:.4f} 秒")
                 self.log("-" * 50)
                 break
             
-            #還沒找到符合的nonce，每十萬次顯示一次目前嘗試次數
-            if nonce % 100000 == 0 and nonce > 0:
-                self.log(f" -已嘗試 {nonce} 次，目前 Hash: {new_hash[:30]}")
+            #還沒找到符合的，每十萬次顯示一次目前次數
+            if attempts % 100000 == 0:
+                self.log(f" - 已嘗試 {attempts} 次，目前Hash前面30位: {new_hash[:30]}")
             
             nonce += 1
+            attempts += 1
             
         self.btn_mine.config(state=tk.NORMAL)
 
